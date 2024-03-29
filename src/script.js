b0869b80c614b529b4e9243cf0c2c8dcaff12a06
j <j@js-MacBook-Pro.local>
2024-03-29 14:26:54 -0700

    got play button working

--- src/script.js
+++ src/script.js
@@ -1,10 +1,20 @@
-function progressButton() {
+const playButton = document.getElementById('play-button');
+const pauseButton = document.getElementById('pause-button');
+const rewindButton = document.getElementById('rwd-button');
+const fastforwardButton = document.getElementById('ff-button');
+const dot = document.getElementById('the-dot');
+const progressBar = document.getElementById('the-bar');
+const track = document.querySelector(".current-track");
 
-}
-
-function rewindButton() {}
-function playButton(){
+function displayProgress() {
     
 }
-function pauseButton(){}
-function fastforwardButton(){}
+
+function back() {}
+
+function playTrack(){
+    track.play()
+}
+
+function pauseTrack(){}
+function skip(){}
