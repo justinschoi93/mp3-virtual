eb8b41c265bd4beb77ede1579635b8628ebe30ee
j <j@js-MacBook-Pro.local>
2024-03-29 17:44:46 -0700

    writing out the functions

---
+++ src/albums.js
@@ -0,0 +1,77 @@
+const BlueHawaii = {
+    name: 'Untogether',
+    artist: 'Blue Hawaii',
+    releaseInfo: '2013',
+    albumArt: '',
+    altPhoto: '../images/BlueHawaii.jpg',
+    tracks: [
+        {
+            track: 1,
+            title: 'Follow',
+            length: '5:06',
+            src: '../Blue Hawii/Untogether/01 Follow.m4p'
+        },
+        {
+            track: 2,
+            title: 'Try to Be',
+            length: '3:58',
+            src: '../Blue Hawaii/Untogether/02 Try to Be.m4p'
+        },
+        {
+            track: 3,
+            title: 'In Two',
+            length: '3:54',
+            src: '../Blue Hawaii/Untogether/03 In Two.m4p'
+        },
+        {
+            track: 4,
+            title: 'In Two II',
+            length: '3:54',
+            src: '../Blue Hawaii/Untogether/04 In Two II.m4p'
+        },
+        {
+            track: 5,
+            title: 'Sweet Tooth',
+            length: '3:54',
+            src: '../Blue Hawaii/Untogether/05 Sweet Tooth.m4p'
+        },
+        {
+            track: 6,
+            title: 'Sierra Lift',
+            length: '3:54',
+            src: '../Blue Hawaii/Untogether/06 Sierra Lift.m4p'
+        },
+        {
+            track: 7,
+            title: 'Yours to Keep',
+            length: '3:54',
+            src: '../Blue Hawaii/Untogether/07 Yours to Keep.m4p'
+        },
+        {
+            track: 8,
+            title: 'Daisy',
+            length: '3:54',
+            src: '../Blue Hawaii/Untogether/08 Daisy.m4p'
+        },
+        {
+            track: 9,
+            title: 'Flammarion',
+            length: '3:54',
+            src: '../Blue Hawaii/Untogether/09 Flammarion.m4p'
+        },
+        {
+            track: 10,
+            title: 'Reaction II',
+            length: '3:54',
+            src: '../Blue Hawaii/Untogether/10 Reaction II.m4p'
+        },
+        {
+            track: 11,
+            title: 'The Other Day',
+            length: '3:54',
+            src: '../Blue Hawaii/Untogether/11 The Other Day.m4p'
+        }
+    ]
+};
+
+export default BlueHawaii;
--- src/script.js
+++ src/script.js
@@ -1,10 +1,27 @@
+import BlueHawaii from './albums.js';
+
+// Media Info
+const trackName = document.querySelector('.track');
+const trackArtist = document.querySelector('.artist');
+const trackAlbum = document.querySelector('.album');
+
+// Media Controller
 const playButton = document.getElementById('play-button');
 const pauseButton = document.getElementById('pause-button');
 const rewindButton = document.getElementById('rwd-button');
 const fastforwardButton = document.getElementById('ff-button');
-const dot = document.getElementById('the-dot');
-const progressBar = document.getElementById('the-bar');
-const track = document.querySelector(".current-track");
+const theBar = document.getElementById('the-bar');
+const theDot = document.getElementById('the-dot');
+const albumCover = document.getElementById('album-cover');
+const artistPhoto = document.getElementById('artist-photo');
+
+const track = {
+    name: BlueHawaii.tracks[0].name,
+    artist: BlueHawaii.tracks[0].artist,
+    album: BlueHawaii.tracks[0].album,
+    src: BlueHawaii.tracks[0].src,
+    img: BlueHawaii.albumArt ? BlueHawaii.albumArt : BlueHawaii.altPhoto
+}
 
 function displayProgress() {
     
@@ -18,3 +35,23 @@ function playTrack(){
 
 function pauseTrack(){}
 function skip(){}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    track.name = album.tracks[0].file;
+    track.artist = album.tracks[0].artist;
+    track.album = album.artist
+    track.img = 
+
+    playButton.addEventListener('click', playTrack);
+    pauseButton.addEventListener('click', pauseTrack);
+    rewindButton.addEventListener('click', back);
+    fastforwardButton.addEventListener('click', skip);
+    progressBar.addEventListener('click', displayProgress);
+    theDot.addEventListener('mousedown', () => {
+        track.pause();
+    });
+    theDot.addEventListener('mouseup', () => {
+        track.play();
+    });
+})
